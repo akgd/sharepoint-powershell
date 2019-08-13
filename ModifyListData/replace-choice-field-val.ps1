1f4f61cc1ca9892651ac171bcbe71e74a8a7102f
Andrea Goodson <akgdev@icloud.com>
2019-08-13 13:37:22 +0100

    Return unique vals

--- ModifyListData/replace-choice-field-val.ps1
+++ ModifyListData/replace-choice-field-val.ps1
@@ -1,23 +1,25 @@
 Connect-PnPOnline -Url "https://yourtenant.sharepoint.com/sites/yoursite" -Credentials $yourCreds
 
 $listName = "Project Inventory"
-$choiceFieldIntName = "Alignment"
-$oldVal = "Cultural Competence and Equity"
-$newVal = "Equitable Opportunities and Outcomes"
+$choiceFieldIntName = "Category"
+$oldVal = "foo"
+$newVal = "bar"
 
 $listItems = Get-PnPListItem -List $listName
 
 foreach ($item in $listItems) {
     $itemID = $item["ID"]
     $currentVals = $item[$choiceFieldIntName]
-    $updatedVals = $currentVals -replace $oldVal, $newVal
 
     if ($currentVals -contains $oldVal) {
+        $updatedVals = $currentVals -replace $oldVal, $newVal
+        $uniqueVals = $updatedVals | Sort-Object -unique
+
         $updatedItemData = @{
-            $choiceFieldIntName = $updatedVals
+            $choiceFieldIntName = $uniqueVals
         }
         
-        $update = Set-PnPListItem -List $listName -Identity $itemID -Values $updatedItemData
+        $update = Set-PnPListItem -List $listName -Identity $itemID -Values 
         Write-Host "Updated item ID $itemID"
 
     }
