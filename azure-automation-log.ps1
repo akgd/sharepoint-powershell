ee39ecbfdc27a653a26873310571183880f321c5
Andrea Goodson <akgdev@icloud.com>
2020-04-20 12:42:18 +0100

    Update azure-automation-log.ps1

--- azure-automation-log.ps1
+++ azure-automation-log.ps1
@@ -6,7 +6,11 @@ $logItem = New-Item -Path $tempPath -ItemType File -Name $logName
 "Some update" | Out-File -FilePath $logItem -Append
 "Another update" | Out-File -FilePath $logItem -Append
 
-$spoCreds = Get-AutomationPSCredential -Name "YourCredName"
-$site = "https://YourTenant.sharepoint.com/sites/YourSite"
-Connect-PnPOnline -Url $site -Credentials $spoCreds
-$uploadLog = Add-PnPFile -Path $logItem -Folder "Documents/Logs"
+# If lof file is not empty
+if (Get-Content $logItem) {
+  $spoCreds = Get-AutomationPSCredential -Name "YourCredName"
+  $site = "https://YourTenant.sharepoint.com/sites/YourSite"
+  Connect-PnPOnline -Url $site -Credentials $spoCreds
+  $uploadLog = Add-PnPFile -Path $logItem -Folder "Documents/Logs"
+}
+
